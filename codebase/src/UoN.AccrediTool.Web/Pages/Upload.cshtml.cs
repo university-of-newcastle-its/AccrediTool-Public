a1a07a633c8c4d624e5d2e4313d298b6d0d40cf5
Lachlan Marler <lcm466@newcastle.edu.au>
2021-11-12 15:27:34 +1100

    models are now filled by the uploaded zip

--- codebase/src/UoN.AccrediTool.Web/Pages/Upload.cshtml.cs
+++ codebase/src/UoN.AccrediTool.Web/Pages/Upload.cshtml.cs
@@ -3,14 +3,23 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
+
 using System.Threading.Tasks;
 using System.IO.Compression;
 using System.IO;
-
+using System.Net;
 using System.Linq;
+using System.Collections.Generic; 
+using System.Text.RegularExpressions;  
+
+//OpenXML
 using DocumentFormat.OpenXml;
 using DocumentFormat.OpenXml.Packaging;
 using DocumentFormat.OpenXml.Spreadsheet;
+
+//JSON.net
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 
 using UoN.AccrediTool.Core.Models;
 
@@ -66,6 +75,7 @@ namespace UoN.AccrediTool.Web.Pages
         private void proccessZip()
         {
 
+
             //===Get ProjectData===
 
             ZipArchiveEntry _ProjectData = zip.GetEntry(zipRoot + "ProjectData.xlsx"); // open ProjectData.xlsx
@@ -92,18 +102,148 @@ namespace UoN.AccrediTool.Web.Pages
 
             SpreadsheetDocument _ProgramDataXLSX = getSpreadSheetFromZip(_ProgramData);
 
-            //TODO: getCellValue cannot handle anything but INT, this needs to be fixed
 
-            //_Program.ProgramCode = int.Parse(getCellValue(_ProgramDataXLSX, "ProgramInfo", "A2"));      // set program code
+            _Program.ProgramCode = int.Parse(getCellValue(_ProgramDataXLSX, "ProgramInfo", "A2"));      // set program code
             _Program.Name = getCellValue(_ProgramDataXLSX, "ProgramInfo", "B2");                        // set program name
-            //_Program.FirstTermCode = int.Parse(getCellValue(_ProgramDataXLSX, "ProgramInfo", "C2"));    // set firstTermCode
-            //_Program.MinUnits = int.Parse(getCellValue(_ProgramDataXLSX, "ProgramInfo", "D2"));         // set minUnits
-            //_Program.Duration = int.Parse(getCellValue(_ProgramDataXLSX, "ProgramInfo", "E2"));         // set duration
-            //_Program.MaxYears = int.Parse(getCellValue(_ProgramDataXLSX, "ProgramInfo", "F2"));         // set maxYears
+            _Program.FirstTermCode = int.Parse(getCellValue(_ProgramDataXLSX, "ProgramInfo", "C2"));    // set firstTermCode
+            _Program.MinUnits = int.Parse(getCellValue(_ProgramDataXLSX, "ProgramInfo", "D2"));         // set minUnits
+            _Program.Duration = int.Parse(getCellValue(_ProgramDataXLSX, "ProgramInfo", "E2"));         // set duration
+            _Program.MaxYears = int.Parse(getCellValue(_ProgramDataXLSX, "ProgramInfo", "F2"));         // set maxYears
+            
+        
+            // get all campuses from API
+            List<UoCampusModel> campusModels = JsonConvert.DeserializeObject<List<UoCampusModel>>(getJsonFromAPI("campuses"));
+
+            String campusName = getCellValue(_ProgramDataXLSX, "ProgramInfo", "G2"); // get name of the campus
+
+            //search for campus
+            for(int i = 0; i < campusModels.Count; i++)
+            {
+                if(campusModels[i].Name.Equals(campusName))
+                {
+                    _Program.Campus = campusModels[i];
+                    _Program.CampusId = campusModels[i].CampusId;
+                    break;
+                }
+                  
+            }
+
+            // TODO: these could probs be put into their own function to clean things up abit
+            List<UoProgramCareerModel> programCareerModels = JsonConvert.DeserializeObject<List<UoProgramCareerModel>>(getJsonFromAPI("program-careers"));
+
+            String careerName = getCellValue(_ProgramDataXLSX, "ProgramInfo", "H2");
+            
+            for(int i = 0; i < programCareerModels.Count; i++)
+            {
+                if(programCareerModels[i].Name.Equals(careerName))
+                {
+                    _Program.ProgramCareer = programCareerModels[i];
+                    _Program.ProgramCareerId = programCareerModels[i].ProgramCareerId;
+                    break;
+                }
+                  
+            }
+
+            List<UoFieldOfEducationModel> fieldOfEducationModels = JsonConvert.DeserializeObject<List<UoFieldOfEducationModel>>(getJsonFromAPI("fields-of-education"));
+
+            String fieldOfEducation = getCellValue(_ProgramDataXLSX, "ProgramInfo", "I2");
+
+            for(int i = 0; i < fieldOfEducationModels.Count; i++)
+            {
+                if(fieldOfEducationModels[i].Name.Equals(fieldOfEducation))
+                {
+                    _Program.FieldOfEducation = fieldOfEducationModels[i];
+                    _Program.FieldOfEducationId = fieldOfEducationModels[i].FieldOfEducationId;
+                    break;
+                }
+                  
+            }
+
+            // ===Get Course data===
+            
+            List<String> courseNames = new List<String>();
+
+            foreach (ZipArchiveEntry entry in zip.Entries)
+            {
+                Regex regex = new Regex(@"^"+zipRoot+"Courses/[A-Z]{4}[0-9]{4}[A-Z]?/$");
+
+
+                if(regex.IsMatch(entry.FullName))
+                {
+                    courseNames.Add(entry.FullName.Substring(22).Remove(entry.FullName.Substring(22).Length -1));
+                }
+                
+            }
+
+            List<UoCourseModel> _CourseModels = new List<UoCourseModel>();
+
+            foreach(String name in courseNames)
+            {
+                ZipArchiveEntry _CourseData = zip.GetEntry(zipRoot +"Courses/" +name+ "/CourseData.xlsx"); // open CourseData.xlsx
+                SpreadsheetDocument _CourseDataXLSX = getSpreadSheetFromZip(_CourseData);
+                UoCourseModel _Course = new UoCourseModel();
+
+                _Course.Subject = name.Remove(4);                                                   // set course subject
+                _Course.CatalogNumber = name.Substring(4);                                          // set course catalogNumber
+                _Course.Position = int.Parse(getCellValue(_CourseDataXLSX, "CourseInfo", "A2"));    // set course position
+                _Course.Name = getCellValue(_CourseDataXLSX, "CourseInfo", "B2");                   // set course name
+                _Course.Description = getCellValue(_CourseDataXLSX, "CourseInfo", "C2");            // set course description
+                _Course.Units = int.Parse( getCellValue(_CourseDataXLSX, "CourseInfo", "D2"));      // set course units
+                
+                List<UoAcademicOrgModel> academicOrgModels =  JsonConvert.DeserializeObject<List<UoAcademicOrgModel>>(getJsonFromAPI("academic-orgs"));
+
+                String academicOrgName = getCellValue(_CourseDataXLSX, "CourseInfo", "E2");
+
+                for(int i = 0; i < academicOrgModels.Count; i++)
+                {
+                    if(academicOrgName.Equals(academicOrgModels[i].Name))
+                    {
+                        _Course.AcademicOrg = academicOrgModels[i];
+                        _Course.AcademicOrgId = academicOrgModels[i].AcademicOrgId;
+                        break;
+                    }
+                }
+
+            
+                fieldOfEducation = getCellValue(_CourseDataXLSX, "CourseInfo", "F2");
+
+                for(int i = 0; i < fieldOfEducationModels.Count; i++)
+                {
+                    if(fieldOfEducationModels[i].Name.Equals(fieldOfEducation))
+                    {
+                        _Course.FieldOfEducation = fieldOfEducationModels[i];
+                        _Course.FieldOfEducationId = fieldOfEducationModels[i].FieldOfEducationId;
+                        break;
+                    }
+                    
+                }
+
+                _CourseModels.Add(_Course);
+
+
+            }
+
+
+
+            //===Post to api===
+
+
+
 
             
         }
 
+        private String getJsonFromAPI(String url)
+        {
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create("http://localhost:53924/api/" + url);
+
+            request.Accept = "application/json";
+            
+
+            StreamReader APIReader = new StreamReader(request.GetResponse().GetResponseStream());
+
+            return APIReader.ReadToEnd();
+        }
 
         private SpreadsheetDocument getSpreadSheetFromZip(ZipArchiveEntry zipEntry)
         {
@@ -130,8 +270,33 @@ namespace UoN.AccrediTool.Web.Pages
 
             Cell cell = sheetPart.Worksheet.Descendants<Cell>().Where(c => c.CellReference == cellLocation).FirstOrDefault(); // get the cell from the sheet part
 
-            // this statment is very temp, only handles strings
-            return excel.WorkbookPart.GetPartsOfType<SharedStringTablePart>().FirstOrDefault().SharedStringTable.ElementAt(int.Parse(cell.InnerText)).InnerText; // return the text stored in the cell
+
+            if(cell != null)
+            {
+                if(cell.DataType == null)
+                {
+                    return cell.InnerText;
+                }
+                else
+                {
+                    //only strings are handled here atm, other types can be added as needed
+                    switch(cell.DataType.Value)
+                    {
+                        case CellValues.SharedString:
+                            return excel.WorkbookPart.GetPartsOfType<SharedStringTablePart>().FirstOrDefault().SharedStringTable.ElementAt(int.Parse(cell.InnerText)).InnerText; // return the text stored in the cell
+                        default:
+                            return "Cell type not found";
+                    }
+                }
+
+
+            }
+            else
+            {
+                return "Cell not found";
+                //cell not found error
+            }
+
         }
     }
 }
